a4b6f38d4e9a26e0fd6e4925e5d1c10d52d85a5f
anuj846k <anuj846k@gmail.com>
2025-07-05 10:32:36 +0530

    Refactor main.py to use ChatGoogleGenerativeAI and implement async session handling with MCP tools for enhanced functionality

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ from dotenv import load_dotenv
 import os
 from mcp import ClientSession , StdioServerParameters
 from mcp.client.stdio import stdio_client
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 
 
 
 load_dotenv()
 print(os.getenv("GEMINI_API_KEY"))
 
-llm = GoogleGenerativeAI(
-    
-)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GEMINI_API_KEY"))
 
 stdio_server_params = StdioServerParameters(
     command="python",
@@ -22,8 +21,17 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
-
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read,write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            # print(tools)
+            
+            agent = create_react_agent(llm,tools)
+            
+            result = await agent.ainvoke({"messages":[HumanMessage(content = 'What is 5 + 2 * 4 ?')]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
         asyncio.run(main())
